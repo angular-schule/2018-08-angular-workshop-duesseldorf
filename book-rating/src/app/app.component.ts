946d35984370162fac1f37a2eed7e50cdccb0dba
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-08-30 16:32:36 +0200

    Property Binding für a href

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Book Rating';
-
+  url = 'https://angular.schule';
+  
   /*constructor() {
     setTimeout(() => this.title = 'HALLO', 2000);
   }*/
