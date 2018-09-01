3a019ac361e68366995cd98ed98f9f6c42a0399a
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-01 13:30:15 +0200

    BookStoreService getAllStatic()

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
 
-  constructor() {}
+  constructor(private bs: BookStoreService) {}
   
   addBookToList(book: Book) {
     this.books = [...this.books, book];
@@ -37,26 +38,7 @@ export class DashboardComponent implements OnInit {
 
 
   ngOnInit() {
-    this.books = [
-      {
-        isbn: '000',
-        title: 'Angular',
-        description: 'Grundlagen, fortgeschrittene Techniken,',
-        rating: 5
-      },
-      {
-        isbn: '111',
-        title: 'React',
-        description: 'Ein anderes Framework',
-        rating: 3
-      },
-      {
-        isbn: '222',
-        title: 'Vue.js',
-        description: 'Für Hipster',
-        rating: 4
-      }
-    ];
+    this.books = this.bs.getAllStatic();
   }
 
   trackBook(index: number, item: Book) {
